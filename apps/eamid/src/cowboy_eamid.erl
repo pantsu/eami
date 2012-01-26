271a6d5ea9e9492aaeee873c65a745f1b4ff0875
komm <komm@siphost.su>
2012-01-26 17:04:56 +0400

    configure ciscophone

--- apps/eamid/src/cowboy_eamid.erl
+++ apps/eamid/src/cowboy_eamid.erl
@@ -9,6 +9,20 @@ pp(P,List)->
                 _->error
         end
 .
+bd()->
+[
+{"04C5A4B1C802","109.235.162.252"},
+{"B8BEBF22A804","109.235.162.250"},
+{"B8BEBF2316F8","109.235.162.249"},
+{"B8BEBF2264E3","109.235.162.253"},
+{"00215553447A","109.235.162.248"},
+{"001FCAE80223","109.235.162.247"},
+{"001E4AF2E16F","109.235.162.245"},
+{"001E4AF255AF","109.235.162.246"},
+{"081FF3636EBF","109.235.162.254"},
+{"B8BEBF22ACBE","111.111.111.111"}
+]
+.
 
 union([])->[];
 union([H|Tail])->
@@ -30,6 +44,28 @@ freecall()->
 		|| X <- qcalls:get(), X#newchannel.link=:=none]
 .
 
+template(From,To)->
+io_lib:format('Channel: SIP/callman61/~s
+Callerid: ~s
+MaxRetries: 0
+RetryTime: 300
+WaitTime: 45
+Context: komm
+Extension: ~s
+Priority: 1
+',[To,From,From]).
+call(error,_)->ok;
+call(_,error)->ok;
+call(From,To)->
+	CText=io_lib:format('~s',[template(From,To)]),
+	Rnd="call_"++integer_to_list(random:uniform(9999999999)),
+	Filename="/tmp/"++ Rnd,
+	SpoolOut="/usr/local/asterisk-1.8.3.2/var/spool/asterisk/outgoing/" ++ Rnd,
+	{ok,S}=file:open(Filename,write),
+	file:write(S,CText),
+	file:close(S),
+	file:rename(Filename,SpoolOut)
+.
 
 init({tcp, http}, Req, Opts) ->
     {ok, Req, undefined_state}.
@@ -51,23 +87,44 @@ handle(Req, State) ->
 				case pp("action",P) of
 					"status"-> 
 						Queue=union(qcalls:get())++freecall() ,
-						io:format('[DEBUG]: [status]: ~w~n',[Queue]),
 						list_to_binary(json2:encode({struct,[{status,{array,[{struct,X}||X<-Queue ]}}]}))
 					
 					;
 					"call"->
-						case pp(channelid,P) of
-							[]->catch spawn(pooler,activeaction,[integer_to_list(pp(number,P)),[]]);
-							_->catch spawn(pooler,activeaction,[integer_to_list(pp(number,P)),pp(channelid,P)])
-						end,
-						{html,"{\"action\":\"ok\"}"}
+						call(pp("from",P),pp("to",P)),
+                                                list_to_binary("{\"action\":\"ok\"}")
 					;
                                         "redirect"->
 						%%{"action":"redirect","channel":"_CHANEL","number":"66666"}
                                                 active_action:redirect(pp("channel",P),pp("number",P)),
                                                 list_to_binary("{\"action\":\"ok\"}")
                                         ;
-     
+					"set_device"->
+						erlydtl:compile("/usr/local/unison/eami/www/cisco7940.template", cisco7940),
+						erlydtl:compile("/usr/local/unison/eami/www/cisco7942.template", cisco7942),
+						{ok,Cisco79xx}=
+						case pp("model",P) of
+							"Cisco 7940"-> cisco7940:render([{number,pp(number,P)}]);
+							"Cisco 7942"-> cisco7940:render([{number,pp(number,P)}]);
+							_->{ok,nodevice}
+						end,
+						io:format('~s~n',[lists:flatten(Cisco79xx)])	,
+						Mac=string:to_upper(pp("macaddr",P)--":::::::"),
+						Filename="/tftpboot/SIP"++Mac++".cnf",
+						io:format('[FILENAME]: ~s~n',[Filename]),
+						file:write_file(Filename,lists:flatten(Cisco79xx)),
+						io:format('[IPPHONE]:~s  ~s~n~w~n',[ Mac,pp(Mac,bd()),bd()]),
+						case pp("model",P) of
+							"Cisco 7940"-> 
+								catch spawn(os,cmd,["/usr/local/unison/eami/www/cisco7940.expect "++pp(Mac,bd())])
+							;
+							"Cisco 7942"-> 
+								catch spawn(os,cmd,["/usr/local/unison/eami/www/cisco7942.expect "++pp(Mac,bd())])
+							;
+							_->{ok,nodevice}
+						end,
+						list_to_binary("{\"action\":\"ok\"}")
+					;     
 					"chan_spy"->
 						catch spawn(pooler,chan_spy,[pp(channel,P),pp(number,P)]),
 						{html,"{\"action\":\"ok\"}"}
